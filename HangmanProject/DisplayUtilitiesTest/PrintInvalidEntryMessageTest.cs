ec971f01cbfa73dea9c6adeb2f8464c5c48fff4e
borislavjivkov <b.jivkov@gmail.com>
2013-05-15 19:33:24 +0300

    Added some tests to DisplayUtilitiesTests. Added xml documentation to Hangman. Did some refactorings in Hangman.

---
+++ HangmanProject/DisplayUtilitiesTest/PrintInvalidEntryMessageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Hangman;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DisplayUtilitiesTest
+{
+    [TestClass]
+    public class PrintInvalidEntryMessageTest
+    {
+        [TestMethod]
+        public void PrintInvalidEntryMessage()
+        {
+            string actual;
+            var originalConsoleOut = Console.Out;
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                DisplayUtilities.PrintInvalidEntryMessage();
+                writer.Flush();
+                actual = writer.GetStringBuilder().ToString();
+            }
+
+            Console.SetOut(originalConsoleOut);
+            string expectedOutput = "Incorrect guess or command!" + Environment.NewLine;
+            Assert.AreEqual(expectedOutput, actual);
+        }
+    }
+}
---
+++ HangmanProject/DisplayUtilitiesTest/PrintWelcomeMessageTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Hangman;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DisplayUtilitiesTest
+{
+    [TestClass]
+    public class PrintWelcomeMessageTest
+    {
+        [TestMethod]
+        public void PrintWelcomeMessage()
+        {
+            string actual;
+            var originalConsoleOut = Console.Out;
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                DisplayUtilities.PrintWelcomeMessage();
+                writer.Flush();
+                actual = writer.GetStringBuilder().ToString();
+            }
+
+            Console.SetOut(originalConsoleOut);
+            string expectedOutput =
+                "Welcome to “Hangman” game. Please try to guess my secret word." + Environment.NewLine +
+                "Use 'top' to view the top scoreboard, 'restart' to start a new game, " + 
+                "'help' to cheat and 'exit' to quit the game." + Environment.NewLine;
+            Assert.AreEqual(expectedOutput, actual);
+        }
+    }
+}
